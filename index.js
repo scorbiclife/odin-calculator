d0a6436ecb163bacdb6066b18c0ac53aece19db5
scorbiclife <scorbiclife+git@gmail.com>
2025-03-19 20:46:04 +0900

    feat: add initial calculator engine implementation
    - [x] round answers with long decimals
    - [x] "clear" wipes out all data
    - [x] when inputting consecutive operators, the last one takes effect
    - [x] pressing a new digit should clear the result
    - [ ] error when divided by zero
    - [ ] floating point

--- index.js
+++ index.js
@@ -1,13 +1,99 @@
-const resultDisplayElement = document.querySelector("#result");
+const getLastOutput = (lastOutput, _currentInput) => lastOutput;
+const replaceWithCurrentInput = (_lastOutput, currentInput) => currentInput;
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
 
-let result = 0;
-function populateDisplay() {
-  resultDisplayElement.textContent = Number.isInteger(result)
-    ? String(result)
-    : result.toFixed(5);
+let operator = replaceWithCurrentInput;
+let lastOutput = 0;
+let currentInput = [];
+let lastInputType = "operator"; /* "number" or "operator" */
+
+function clear() {
+  operator = replaceWithCurrentInput;
+  lastOutput = 0;
+  currentInput = [];
+  lastInputType = "operator";
 }
 
+function inputDigit(digitInput) {
+  currentInput.push(digitInput);
+  lastInputType = "number";
+}
+
+function inputOperator(operatorInput) {
+  if (lastInputType !== "number" && lastInputType !== "operator") {
+    throw new Error("Invalid state!");
+  }
+  if (lastInputType === "number") {
+    const currentInputNumber =
+      currentInput.length === 0 ? 0 : Number(currentInput.join(""));
+    lastOutput = operator(lastOutput, currentInputNumber);
+  }
+  currentInput = [];
+  operator = operatorInput;
+  lastInputType = "operator";
+}
+
+function formatNumber(x) {
+  return Number.isInteger(x) ? String(x) : x.toFixed(5);
+}
+
+function getTextToDisplay() {
+  if (lastInputType === "number") {
+    return currentInput.join("");
+  }
+  if (lastInputType === "operator") {
+    return formatNumber(lastOutput);
+  }
+  throw new Error("Invalid state!");
+}
+
+const resultDisplayElement = document.querySelector("#result");
+function populateDisplay() {
+  resultDisplayElement.textContent = getTextToDisplay();
+}
+
+for (let i = 0; i <= 9; ++i) {
+  document.querySelector(`#number-${i}`)?.addEventListener("click", () => {
+    inputDigit(`${i}`);
+    populateDisplay();
+  });
+}
+
+document.querySelector("#symbol-plus")?.addEventListener("click", () => {
+  inputOperator(add);
+  populateDisplay();
+});
+
+document.querySelector("#symbol-minus")?.addEventListener("click", () => {
+  inputOperator(subtract);
+  populateDisplay();
+});
+
+document.querySelector("#symbol-times")?.addEventListener("click", () => {
+  inputOperator(multiply);
+  populateDisplay();
+});
+
+document.querySelector("#symbol-division")?.addEventListener("click", () => {
+  inputOperator(divide);
+  populateDisplay();
+});
+
+document.querySelector("#symbol-equals")?.addEventListener("click", () => {
+  inputOperator(getLastOutput);
+  populateDisplay();
+});
+
+document.querySelector("#control-clear")?.addEventListener("click", () => {
+  clear();
+  populateDisplay();
+});
+
 function init() {
+  clear();
   populateDisplay();
 }
 
